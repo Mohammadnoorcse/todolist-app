4fa9f11bfbb88d51c7b3414bf9aecab04aed7fb8
Mohammad noor <mohammadnoorcse@gmail.com>
2022-06-05 23:32:56 +0600

    new project in react

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+<script
+  src="https://kit.fontawesome.com/abe6634b14.js"
+  crossorigin="anonymous"
+></script>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
